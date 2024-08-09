4cb4f74101ea07d3393674368f36ebfaa5da8e30
ssarwarjahan <ssarwarjahan@gmail.com>
2024-08-09 01:40:54 +0000

    version change

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-0.1.1";
+const CACHE = "pwabuilder-0.1.2";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
